ab36efba80c0e8cd76cd440a900b4252169d17a9
Scott Bradley <scott2b@users.noreply.github.com>
2020-09-08 16:23:51 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 def read_root():
     with open('/usr/src/events/deploy-event', 'w') as f:
         f.write('')
-    return {"Hello": "World"}
+    return {"Hello": "World Test"}
 
 
 @app.get("/items/{item_id}")
